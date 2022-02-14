49dbf62dd41487f684722cba99695cd434671522
Michele Cozzola <cozzolamichele89@gmail.com>
2022-02-14 11:31:50 +0100

    hero & js

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-const nav = document.querySelector('.navbar')
+const nav = document.querySelector('.navbar');
+const menu = document.querySelector('.menu');
+const menuBtn = document.querySelector('.menu-btn')
+
+/* navbar bg */
 document.addEventListener('scroll', ()=>{
     if(scrollY >20){
         nav.classList.add('sticky')
     }else{
         nav.classList.remove('sticky')
     }
+})
+
+menuBtn.addEventListener('click', ()=>{
+    menu.classList.toggle('active')
+    menuBtn.classList.toggle('is-active')
 })
